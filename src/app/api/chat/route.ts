3f27197017877d69dde40cbf7e80915276035706
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-01-11 00:03:05 -0500

    add test page

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import {openai} from "@ai-sdk/openai";
+import {generateText, streamText} from "ai";
+import {NextResponse} from "next/server";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  console.log("Llegandooo");
+
+  return NextResponse.json(
+    {
+      ok: true,
+      status: 200,
+      message: "Hello, world!",
+    },
+    {status: 200},
+  );
+
+  const {messages} = await req.json();
+
+  const {text} = await generateText({
+    model: openai("gpt-4o"),
+    messages,
+  });
+
+  console.log(text);
+
+  return text;
+}
---
+++ src/app/input/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, {useState} from "react";
+
+export default function Chat() {
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({message: input}),
+      });
+      const data = await res.json();
+
+      console.log(data);
+      setResponse(data.message);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="stretch mx-auto flex w-full max-w-md flex-col py-24">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl"
+          placeholder="Say something..."
+          value={input}
+          onChange={handleInputChange}
+        />
+      </form>
+      {response && (
+        <div className="mt-4 rounded border border-gray-300 p-4 shadow-xl">{response}</div>
+      )}
+    </div>
+  );
+}
