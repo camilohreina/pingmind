0091efe4e15f108e85cca2a57e34f85dc1cf8f52
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-02-03 20:06:32 -0500

    fix utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { getCountry } from "countries-and-timezones";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTimeZoneFromCountryCode = (countryCode: string): string => {
+  const country = getCountry(countryCode);
+  if (!country) return "UTC";
+  
+  return country.timezones[0]
+};
