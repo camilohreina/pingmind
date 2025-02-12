31e2f73a9a165993f2a40608b4f0281e950a03e5
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-02-11 23:03:09 -0500

    fix welcome message

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,9 +2,9 @@ export const TRIGGER_REGISTER_WORDS = ["hi", "hola", "buenas", "hello"];
 
 export const AUTO_REPLY_REGISTER = (phone: string) =>
   `¡Hola! 👋 Este es un mensaje de Camilo, el creador de PingMind:\n
-¡Tengo buenas noticias! Ahora puedes probar PingMind gratis por 3 días y descubrir todas sus funcionalidades 🎉
+¡Tengo buenas noticias! Ahora puedes probar pingmind gratis por 3 días y descubrir todas sus funcionalidades 🎉
 Ingresa a la web para registrarte y elegir uno de nuestros planes. Tranqui, puedes cancelar en cualquier momento 🤝
-https://www.pingmind.vercel.app/es/signup?phone=${phone} \n
+https://pingmind.vercel.app/es/signup?phone=${phone} \n
 Una vez tengas un plan, vuelve a esta conversación y envía un mensaje para seguir conversando.\n
 ¡Gracias por ser parte de esta aventura! 🚀`;
 
