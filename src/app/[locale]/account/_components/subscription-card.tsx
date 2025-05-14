a3224edc6b57c0e8aef0ccfb699a749b4c22bd1d
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-05-14 13:25:46 -0500

    feat: enhance subscription card with internationalization support and update translations

--- src/app/[locale]/account/_components/subscription-card.tsx
+++ src/app/[locale]/account/_components/subscription-card.tsx
@@ -8,8 +8,8 @@ import {
 } from "@/components/ui/card";
 import { Link } from "@/i18n/routing";
 import { getUserSubscriptionPlan } from "@/lib/lemonsqueezy";
+import { useTranslations } from "next-intl";
 import { format } from "date-fns";
-import { ArrowRight } from "lucide-react";
 import React from "react";
 
 type Props = {
@@ -17,13 +17,15 @@ type Props = {
 };
 
 export default function SubscriptionCard({ subscription }: Props) {
+  const t = useTranslations("account_page.subscription_card");
+
   return (
     <Card className="w-full">
       <CardHeader>
-        <CardTitle className="text-2xl text-left">Subscription Plan </CardTitle>
+        <CardTitle className="text-2xl text-left">{t("title")}</CardTitle>
         <CardDescription>
           <p className="text-left">
-            You are currently on the <strong>{subscription.name}</strong> plan.
+            {t("description", { planName: subscription.name })}
           </p>
         </CardDescription>
       </CardHeader>
@@ -32,16 +34,15 @@ export default function SubscriptionCard({ subscription }: Props) {
         {subscription.isSubscribed ? (
           <p className="text-xs font-medium">
             {subscription.isCanceled
-              ? "Your subscription is canceled on "
-              : "Your plan renews on "}
-            {format(subscription.stripe_current_period_end!, "dd.MM.yyyy")}
+              ? t("cancellation", { date: format(subscription.stripe_current_period_end!, "dd.MM.yyyy") })
+              : t("renewal", { date: format(subscription.stripe_current_period_end!, "dd.MM.yyyy") })}
           </p>
         ) : null}
         <Button asChild variant="secondary" className="w-full mt-3" size="sm">
           {subscription.portalUrl ? (
-            <Link href={subscription.portalUrl}>Manage your subscription</Link>
+            <Link href={subscription.portalUrl}>{t("manage_subscription")}</Link>
           ) : (
-            <Link href="/plans">Upgrade your plan</Link>
+            <Link href="/plans">{t("upgrade_plan")}</Link>
           )}
         </Button>
       </CardContent>
