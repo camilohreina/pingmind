951eb87a1042336e2adef397188700b39395ddb1
Camilo Hernandez <camilohreina.dev@gmail.com>
2025-05-20 21:36:07 -0500

    fix: update live variant IDs for Starter and Pro plans

--- src/config/pricing.ts
+++ src/config/pricing.ts
@@ -11,7 +11,7 @@ export const PLANS = [
         variantId: "683605",
       },
       live: {
-        variantId: "683605",
+        variantId: "812713",
       },
     },
     price: {
@@ -34,7 +34,7 @@ export const PLANS = [
         variantId: "683622",
       },
       live: {
-        variantId: "683622",
+        variantId: "812714",
       },
     },
     price: {
